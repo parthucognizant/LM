528282020a553c3c4d8598fccb3fb07184d274fb
parthucognizant <pardhu0548@gmail.com>
2016-02-24 10:23:01 +0530

    initial commit

---
+++ LibraryManager/src/com/example/librarymanager/util/constants.java
@@ -0,0 +1,13 @@
+package com.example.librarymanager.util;
+
+public class constants {
+
+	public static final class logs{
+
+		public static String mainOncreate = "Mainactivity-oncreate";
+		public static String signupClick = "Mainactivity-signup";
+		public static String loginClick = "Mainactivity-login";
+
+	}
+
+}
